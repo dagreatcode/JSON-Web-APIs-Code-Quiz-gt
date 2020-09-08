6d9fe7bf9c77468a00bdf89be2cf01060969fe22
dagreatcode <dagreatcode@gmail.com>
2020-09-07 23:56:09 -0400

    just getting together my timer

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log(document)
+var countdownElement = document.getElementById("countdown");
+var textElementById("text");
+
+function prepareQuiz() {
+    var timer 10;
+    var  prepareInterval = setInterval(function () {
+        timer--;
+        countdownElement.textContent = timer + " seconds until we start.";
+        if(timer === 0) {
+            clearInterval(prepareInterval);
+            countdownElement.textContent = "";
+            
+        }
+    })
+}
+
+console.log()
