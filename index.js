2b5b38da2f2b4e5c089454510c1f8007fd8137d2
dagreatcode <dagreatcode@gmail.com>
2020-09-09 18:37:49 -0400

    taking a slower path

---
+++ index.js
@@ -0,0 +1,13 @@
+var currentQuestion = 0;
+var score = 0;
+
+var form = document.getElementById("form");
+var questionElement = document.getElementById("question-text");
+var questionText = document.getElementById("text");
+var buttonOne = document.getElementById("buttonOne");
+var buttonTwo = document.getElementById("buttonTwo");
+var buttonThree = document.getElementById("buttonThree");
+var buttonFour = document.getElementById("buttonFour");
+var nextButton = document.getElementById("submit");
+var resultCount = document.getElementById("text")
+
