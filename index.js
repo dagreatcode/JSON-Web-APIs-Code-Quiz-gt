1b73a4a44849ee8aeb94e92ad7d01001cf06c082
dagreatcode <dagreatcode@gmail.com>
2020-09-09 18:53:47 -0400

    The simple way... adding my variables globle so i can always look up

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+//Adding my Variables
 var currentQuestion = 0;
 var score = 0;
 
 var form = document.getElementById("form");
-var questionElement = document.getElementById("question-text");
+var questionElement = document.getElementById("allQuestions");
 var questionText = document.getElementById("text");
 var buttonOne = document.getElementById("buttonOne");
 var buttonTwo = document.getElementById("buttonTwo");
 var buttonThree = document.getElementById("buttonThree");
 var buttonFour = document.getElementById("buttonFour");
 var nextButton = document.getElementById("submit");
-var resultCount = document.getElementById("text")
+var resultYnN = document.getElementById("question-text")
+var resultCount = document.getElementById("result")
 
+function loadQuestion
