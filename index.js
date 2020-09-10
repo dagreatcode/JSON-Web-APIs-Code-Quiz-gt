c6a708cfe7a0ed70f058f733411fea17f5eeba2d
dagreatcode <dagreatcode@gmail.com>
2020-09-09 21:26:37 -0400

    fixing on a function

--- index.js
+++ index.js
@@ -13,21 +13,67 @@ var buttonFour = document.getElementById("buttonFour");
 var nextButton = document.getElementById("submit");
 var resultYnN = document.getElementById("question-text");
 var resultCount = document.getElementById("result");
+var question = [
+    "Question1",
+    "Question2", 
+    "Question3",
+    "Question4", 
+    "Question5", 
+    "Question6", 
+];
+
+function nextQuestion (allQuestionsIndex) {
+    var addd = question[allQuestionsIndex];
+    questionElement.textContent = (questionIndex + 1) + ', '+ addd.allQuestions;
+    buttonOne.textContent = addd.buttonOne;
+    buttonTwo.textContent = addd.buttonTwo;
+    buttonThree.textContent = addd.buttonThree;
+    buttonFour.textContent = addd.buttonFour;
+}
+
 
 // //Adding my Functions
 function prepareQuiz() {
 //My Nice Timer
-    // var timer = 10;
+    var timer = 10;
     var prepareInterval = setInterval(function () {
        timer--;
        countdownElement.textContent = timer + " seconds until we start.";
        if(timer === 0) {
            clearInterval(prepareInterval);
-           countdownElement.textElement = "";
+           countdownElement.textElement = 0;
            prepareQuiz();
-           console.log(prepareQuiz);
         }
     }, 1000);
 }
 
-console.log("hello")
+console.log("hello");
+
+//ButtonEvents 
+
+}
+
+buttonOne.addEventListener("click", function() {
+
+})
+
+buttonOne.addEventListener("click", function() {
+    
+})
+
+buttonOne.addEventListener("click", function() {
+    
+})
+
+buttonOne.addEventListener("click", function() {
+    
+})
+
+
+//Test Runner
+function runTest() {
+    for (var i = 0; i < runTest.length; i++) {
+        var runTest = runTest[i];
+        if click === true;
+
+    }
