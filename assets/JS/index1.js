36a0a58f743e6800fe67317dc068d9829b8e1204
dagreatcode <dagreatcode@gmail.com>
2020-12-10 19:37:11 -0500

    updating my new index.js file to show question in the console

--- assets/JS/index1.js
+++ assets/JS/index1.js
@@ -11,28 +11,35 @@ prepareQuiz();
 function prepareQuiz() {
     //My Nice Timer
     var timerInterval = setInterval(function () {
-    timer2--;
-    countdownElement.textContent = timer + " seconds until we start.";
+    timer--;
+    countdownElement.textContent = timer + " seconds until we start the Code Quiz.";
         if(timer === 0) {
             clearInterval(timerInterval);
-            // startTimer();
-            // startQuiz();
+            startTimer();
+            startQuiz();
         }
     }, 1000);
 };
 
 function startTimer() {
     var timerInterval = setInterval(function () {
-    timer--;
-    countdownElement.textContent = timer + "";
-        if(timer === 0) {
+    timer2--;
+    countdownElement.textContent = timer2 + " seconds until finished";
+        if(timer2 === 0) {
             clearInterval(timerInterval);
             // prepareQuiz();
         }
     }, 1000);
 };
 
-function startQuiz() {
+var questions = [
+    "Question1 A,B,C, or D",
+    "Question2 A,B,C, or D", 
+    "Question3 A,B,C, or D",
+    "Question4 A,B,C, or D", 
+    "Question5 A,B,C, or D", 
+];
 
+function startQuiz() {
+console.log("randomQuestionArray", questions);
 };
-    
